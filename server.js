5424a180f70499bbc2c5d1b820cbeca85e353b9a
Juan Ospina <>
2022-07-25 13:32:46 -0500

    update test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Snyk Codelab Failure');
+  res.send('Snyk Codelab Success');
 });
 
 app.get('/health', (req, res) => {
