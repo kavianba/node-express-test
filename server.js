771af15bc5a84dcdf24666f7675ead203bdafcf5
Juan Ospina <>
2022-07-27 17:25:38 -0500

    another run

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Snyk Codelab Rerun 2');
+  res.send('Snyk Codelab Verify TINT-148');
 });
 
 app.get('/health', (req, res) => {
